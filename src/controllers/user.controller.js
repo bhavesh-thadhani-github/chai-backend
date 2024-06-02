ae826b1b4cd758b65584a0b110df3e29b437c224
bhavesh-thadhani-github <bhaveshthadhani2@gmail.com>
2024-06-02 14:01:28 +0530

    Add user account management endpoints || writing update controller for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -323,4 +323,107 @@ const refreshAcessToken = asyncHandler(async (req, res) => {
   }
 })
 
-export { registerUser, loginUser, logoutUser, refreshAcessToken };
+//to change the current user password
+const changeCurrentUser = asyncHandler(async (req, res) => {
+  const {oldPassword, newPassword} = req.body
+
+  //the user is able to change his password this shows that the user is loggedIn
+  //and bcoz of middleware the user is in the req.user & we can extract the user id from that
+  const user = await User.findById(req.user?._id)
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword, this.password)
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, 'Invalid old password')
+  }
+
+  user.password = newPassword
+  await user.save({validateBeforeSave: false})
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, {}, 'Password Changed Successfully'))
+
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+  .status(200)
+  .json(200, {}, 'Current User Fetched Successfully')
+})
+
+const updateAccountDetails = asyncHandler(async (res, res) => {
+  //we should create a separate controller to edit or update a file
+  const {fullName, email} = req.body
+
+  if (!fullName || !email) {
+    throw new error(400, 'All Fields are Required')
+  }
+
+  const user = User.findByIdAndUpdate(req.user?._id, {$set: {
+    fullName,
+    email: email
+  }}, 
+  {new: true}).select('-password ')
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, 'Account details updated successfully'))
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, 'Avatar file is missing')
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if (!avatar.url) {
+    throw new ApiError(400, 'Error while uploading on avatar')
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url
+      }
+    },
+    {new: true}
+  ).select('-password')
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, 'Avatar image updated successfully'))
+})
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, 'Cover image is missing')
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!coverImage.url) {
+    throw new ApiError(400, 'Error while uploading on cover image')
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    {new: true}
+  ).select('-password')
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, 'Cover image updated successfully'))
+})
+
+export { registerUser, loginUser, logoutUser, refreshAcessToken, changeCurrentUser, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage };
